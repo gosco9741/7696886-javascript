b37fba5f8fb5fdcc682103362de7b7391b8b38d5
gosco9741 <146781712+gosco9741@users.noreply.github.com>
2024-02-19 23:27:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,11 @@
 const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
 let score = 0
 
-// Déclaration de la variable contenant le mot saisi par l'utilisateur
-// Le prompt sert à afficher une popup demandant à l'utilisateur de saisir un mot
-let motUtilisateur = prompt('Entrez le mot : ' + listeMots[0])
-
-// Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-if (motUtilisateur === listeMots[0]) {
-    score++
+For (let i=0; i<3; i++){
+let motUtilisateur = prompt("Entrer le mot " + listeMots[i])
+if (entrer === listeMots[i]){
+    score+=1;
 }
-
-// Idem avec le second mot du tableau
-// Notez l'absence du "let" devant la déclaration de la variable "motUtilisateur", cela permet de réutiliser la variable déclarée précédemment
-motUtilisateur = prompt('Entrez le mot : ' + listeMots[1])
-if (motUtilisateur === listeMots[1]) {
-    score++
-}
-
-// Idem avec le troisième mot du tableau
-motUtilisateur = prompt('Entrez le mot : ' + listeMots[2])
-if (motUtilisateur === listeMots[2]) {
-    score++
 }
 
 // Affichage du score de l'utilisateur
